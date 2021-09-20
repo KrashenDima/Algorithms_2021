4f8fa826182e1b8f0e27bf2c7ffcdf22d78d5c4c
KEN <krasheninnikovdima30@gmail.com>
2021-09-20 18:59:24 +0500

    Adding  a deletion

--- NodeBTS.cpp
+++ NodeBTS.cpp
@@ -21,3 +21,48 @@ Node* insert(Node* node, int data) {
 
 	return node;
 }
+
+// Find the inorder successor 
+Node* minValueNode(Node* node) {
+	Node* current = node;
+
+	// Find the leftmost leaf
+	while (current && current->left != NULL)
+		current = current->left;
+
+	return current;
+}
+
+Node* deleteNode(Node* root, int data) {
+	//Return if the tree is empty
+	if (root == NULL) return root;
+
+	// Find the node to be deleted
+	if (key < root->key)
+		root->left = deleteNode(root->left, key);
+	else if (key > root->key)
+		root->right = deleteNode(root->right, key);
+	else {
+		// If the node is with only one child or no child
+		if (root->left == NULL) {
+			struct node* temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL) {
+			struct node* temp = root->left;
+			free(root);
+			return temp;
+		}
+
+		// If the node has two children
+		struct node* temp = minValueNode(root->right);
+
+		// Place the inorder successor in position of the node to be deleted
+		root->key = temp->key;
+
+		// Delete the inorder successor
+		root->right = deleteNode(root->right, temp->key);
+	}
+	return root;
+}
