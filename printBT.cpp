8a55b9c2bf732d6a487a9d639fc2add65462a581
Andrey Vasilev <39232744+NGdre@users.noreply.github.com>
2021-09-20 14:17:07 +0300

    adds print binary search tree func

---
+++ printBT.cpp
@@ -0,0 +1,24 @@
+﻿#include "printBT.h"
+
+
+void printBT(const std::string& prefix, const Node* node, bool isLeft)
+{
+    if (node != nullptr)
+    {
+        std::cout << prefix;
+
+        std::cout << (isLeft ? "├──" : "└──");
+
+        // print the value of the node
+        std::cout << node->data << std::endl;
+
+        // enter the next tree level - left and right branch
+        printBT(prefix + (isLeft ? "│   " : "    "), node->left, true);
+        printBT(prefix + (isLeft ? "│   " : "    "), node->right, false);
+    }
+}
+
+void printBT(const Node* node)
+{
+    printBT("", node, false);
+}
